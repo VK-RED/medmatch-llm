07fdec069ecd61da173dc89d6c74a4b54c39a36d
Vishnu Kumar <vishnukumart96@gmail.com>
2024-03-05 13:19:21 +0530

    Create an llmClient using OpenAI

---
+++ src/openai.ts
@@ -0,0 +1,111 @@
+import OpenAI from "openai";
+import { WebSocket } from "ws";
+import { RetellRequest, RetellResponse, Utterance } from "./types";
+import { AGENT_PROMPT, BEGIN_SENTENCE, GPT_BEHAVIOR } from "./lib";
+
+export class LlmClient {
+  private client: OpenAI;
+
+  constructor() {
+    this.client = new OpenAI({
+      apiKey: process.env.OPENAI_APIKEY,
+      organization: process.env.OPENAI_ORGANIZATION_ID,
+    });
+  }
+
+  // First sentence requested
+  BeginMessage(ws: WebSocket) {
+    const res: RetellResponse = {
+      response_id: 0,
+      content: BEGIN_SENTENCE,
+      content_complete: true,
+      end_call: false,
+    };
+    ws.send(JSON.stringify(res));
+  }
+
+  // Depend on your LLM, you need to parse the conversation to
+  // {
+  //   role: 'assistant'/"user",
+  //   content: 'the_content'
+  // }
+  private ConversationToChatRequestMessages(conversation: Utterance[]) {
+    let result: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [];
+    for (let turn of conversation) {
+      result.push({
+        role: turn.role === "agent" ? "assistant" : "user",
+        content: turn.content,
+      });
+    }
+    return result;
+  }
+
+  private PreparePrompt(request: RetellRequest) {
+    let transcript = this.ConversationToChatRequestMessages(request.transcript);
+    let requestMessages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] =
+      [
+        {
+          role: "system",
+          // This is the prompt that we add to make the AI speak more like a human
+          content: GPT_BEHAVIOR + AGENT_PROMPT
+        },
+      ];
+    for (const message of transcript) {
+      requestMessages.push(message);
+    }
+    if (request.interaction_type === "reminder_required") {
+      // Change this content if you want a different reminder message
+      requestMessages.push({
+        role: "user",
+        content: "(Now the user has not responded in a while, you would say:)",
+      });
+    }
+    return requestMessages;
+  }
+
+  async DraftResponse(request: RetellRequest, ws: WebSocket) {
+    console.clear();
+
+    if (request.interaction_type === "update_only") {
+      // process live transcript update if needed
+      return;
+    }
+    const requestMessages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] =
+      this.PreparePrompt(request);
+
+    try {
+      const events = await this.client.chat.completions.create({
+        model: "gpt-3.5-turbo-1106",
+        messages: requestMessages,
+        stream: true,
+        temperature: 0.3,
+        frequency_penalty: 1,
+        max_tokens: 200,
+      });
+
+      for await (const event of events) {
+        if (event.choices.length >= 1) {
+          let delta = event.choices[0].delta;
+          if (!delta || !delta.content) continue;
+          const res: RetellResponse = {
+            response_id: request.response_id || 0,
+            content: delta.content,
+            content_complete: false,
+            end_call: false,
+          };
+          ws.send(JSON.stringify(res));
+        }
+      }
+    } catch (err) {
+      console.error("Error in gpt stream: ", err);
+    } finally {
+      const res: RetellResponse = {
+        response_id: request.response_id || 0,
+        content: "",
+        content_complete: true,
+        end_call: false,
+      };
+      ws.send(JSON.stringify(res));
+    }
+  }
+}
