e39041852d564f4c183d38fac21f195047726c87
Vishnu Kumar <vishnukumart96@gmail.com>
2024-03-05 13:24:16 +0530

    Setup index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-function greet(){
-    console.log("INitialised the repo !!!!")
-}
+import dotenv from "dotenv";
+// Load up env file which contains credentials
+dotenv.config({ path: `.env` });
+
+import { Server } from "./server";
+
+const server = new Server();
+server.listen(8080);
